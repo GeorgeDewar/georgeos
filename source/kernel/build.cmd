210fabf695aa47db3fd3324b7c1a3e2541daffb3
George Dewar <george@dewar.co.nz>
2019-12-24 22:25:08 +1300

    Switch to memory model small

--- source/kernel/build.cmd
+++ source/kernel/build.cmd
@@ -5,7 +5,7 @@ rem ndisasm.exe kernel.bin
 rem problem seems to be the stack isn't set right initially
 
 wasm -0 entry.asm
-wcc -3 -d0 -ml -s -wx -zls kernel.c
+wcc -3 -d0 -ms -s -wx -zls kernel.c
 
 wlink FILE entry.obj FILE kernel.obj NAME kernel.bin FORMAT DOS OUTPUT RAW^
   OFFSET=0x0000 OPTION NODEFAULTLIBS^
