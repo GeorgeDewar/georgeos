e4368404c6575851e51d3f4340724d3ea9136366
George Dewar <george@dewar.co.nz>
2019-12-26 23:20:57 +1300

    Restructure, graphics mode, yellow text

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -1,6 +1,4 @@
-void clearScreen();
-void printchar(char c);
-void print(char *s);
+#include "bios/video.c";
 
 char *hello = "Welcome to GeorgeOS!";
 
@@ -11,27 +9,4 @@ void kernelMain(void) {
    while(1==1) {}
 }
 
-void clearScreen() {
-   __asm {
-      mov ah, 0x00
-      mov al, 0x03  ; text mode 80x25 16 colours
-      int 0x10
-   }
-}
 
-void print(char* s) {
-   while(*s != 0) {
-      printchar(*s);
-      s++;
-   }
-}
-
-void printchar(char c) {
-   __asm {
-      mov ah, 0Eh
-      mov al, [c]
-      mov bh, 0
-      mov bl, 0Fh
-      int 10h
-   }
-}
