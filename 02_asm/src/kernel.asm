43bb51eba575e361553eef75e2b8443394e09d8d
George Dewar <george@dewar.co.nz>
2020-01-09 20:35:56 +1300

    Small simplification

--- 02_asm/src/kernel.asm
+++ 02_asm/src/kernel.asm
@@ -14,36 +14,29 @@
 ; START OF MAIN KERNEL CODE
 
 os_main:
-    cli                      ; Clear interrupts
-    mov ax, 0
-    mov ss, ax               ; Set stack segment and pointer
-    mov sp, 0FFFFh
-    sti                      ; Restore interrupts
+    mov ax, 0                   ; Set stack segment to zero
+    mov ss, ax
+    mov sp, 0FFFFh              ; Set stack pointer to the top of the segment
 
-    cld                      ; The default direction for string operations
-                             ; will be 'up' - incrementing address in RAM
+    cld                         ; The default direction for string operations
+                                ; will be 'up' - incrementing address in RAM
 
-    mov ax, 2000h            ; Set all segments to match where kernel is loaded
-    mov ds, ax               ; After this, we don't need to bother with
-    mov es, ax               ; segments ever again, as MikeOS and its programs
+    mov ax, 2000h               ; Set data segment to match where kernel is loaded
+    mov ds, ax
 
-    mov ax, 1003h            ; Set text output with certain attributes
-    mov bx, 0                ; to be bright, and not blinking
-    int 10h
-
-    mov si, welcome_string   ; Put string position into SI
-    call print_string        ; Call our string-printing routine
+    mov si, welcome_string      ; Put string position into SI
+    call print_string           ; Call our string-printing routine
 
     welcome_string db 'Welcome to GeorgeOS, ASM Kernel Edition!', 0
 
-print_string:                ; Routine: output string in SI to screen
-    mov ah, 0Eh              ; int 10h 'print char' function
+print_string:                   ; Routine: output string in SI to screen
+    mov ah, 0Eh                 ; int 10h 'print char' function
 
 .repeat:
-    lodsb                    ; Get character from string
+    lodsb                       ; Get character from string
     cmp al, 0
-    je .done                 ; If char is zero, end of string
-    int 10h                  ; Otherwise, print it
+    je .done                    ; If char is zero, end of string
+    int 10h                     ; Otherwise, print it
     jmp .repeat
 
 .done:
