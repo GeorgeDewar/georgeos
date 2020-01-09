61ad120d5c6f31b000fac9e900858e5325563bff
George Dewar <george@dewar.co.nz>
2020-01-09 20:17:20 +1300

    Tidy entry.asm, replace 02 bootloader

--- current/src/kernel/entry.asm
+++ current/src/kernel/entry.asm
@@ -1,30 +1,20 @@
 .model small
 .code ENTRY
-extern kernelMain_ : proc
+    extern kernelMain_ : proc
 
-	cli				; Clear interrupts
-	mov ax, 0
-	mov ss, ax			; Set stack segment and pointer
-	mov sp, 0FFFFh
-	sti				; Restore interrupts
+    mov ax, 0               ; Set stack segment to zero
+    mov ss, ax
+    mov sp, 0FFFFh          ; Set stack pointer to the top of the segment
 
-	cld				; The default direction for string operations
-					; will be 'up' - incrementing address in RAM
+    cld                     ; Ensure the direction for string operations will be 'up' - incrementing address in RAM
 
-	mov ax, 2100h			; Set all segments to match where kernel is loaded
-	mov ds, ax			; After this, we don't need to bother with
-	mov es, ax			; segments ever again, as MikeOS and its programs
+    mov ax, 2100h           ; Set segments (DS and ES) to the right location
+    mov ds, ax
+    mov es, ax
 
-	mov ax, 1003h			; Set text output with certain attributes
-	mov bx, 0			; to be bright, and not blinking
-	int 10h
-
-	mov ah, 0Eh
+    mov ah, 0Eh             ; Print a dot for debugging purposes
     mov al, '.'
-    mov bh, 0
-    mov bl, 0Fh
     int 10h
 
-jmp kernelMain_
-db 0x90
+    jmp kernelMain_         ; Jump to the main function in our C code
 end
