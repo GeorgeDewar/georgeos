e8def352053433febdb2ede284a949812b04ff8c
George Dewar <george@dewar.co.nz>
2020-01-22 22:20:34 +1300

    Tidy more

--- current/src/programs/test-watcom/entry.asm
+++ current/src/programs/test-watcom/entry.asm
@@ -2,21 +2,11 @@
 .code ENTRY
     extern appMain_ : proc
 
-    mov ah, 0Eh             ; Print a dot for debugging purposes
-    mov al, '.'
-    int 10h
+    mov ax, cs                  ; Add CS (the segment to which this app was loaded)
+    add ax, @data               ; to @data (the data segment offset of this app)
+    mov ds, ax                  ; Set the result as DS
+    
+    call appMain_               ; Jump to the main function in our C code
 
-    mov ax, cs
-    add ax, @data
-    ;add ax, 4000h           ; 0x2000 seg for kernel, plus 0x8000 offset for app
-    mov ds, ax
-    ; mov es, ax
-
-    call appMain_            ; Jump to the main function in our C code
-
-    mov ah, 0Eh             ; Print a dot for debugging purposes
-    mov al, '/'
-    int 10h
-
-    retf
+    retf                        ; Far return to the OS (must be a far return for a far call)
 end
