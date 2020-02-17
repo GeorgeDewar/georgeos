33d1bc2bf3f7565f731cd7067a028e0f4c9141c2
George Dewar <george@dewar.co.nz>
2020-02-17 21:27:33 +1300

    Tidy file

--- youtube/video01_hi/bootload.asm
+++ youtube/video01_hi/bootload.asm
@@ -1,16 +1,17 @@
-BITS 16
-org 0x7C00
+org 0x7C00 ; Tell the assembler where this code will be loaded
 
-mov ah, 0Eh
-mov bh, 0
+mov ah, 0x0E                    ; Function code
+mov bh, 0                       ; Page number
 
-mov al, 'H'
-int 10h
+; Write an H to the screen
+mov al, 'H'                     ; Move character code into AL
+int 10h                         ; Invoke BIOS function
 
+; Write an I to the screen
 mov al, 'i'
 int 10h
 
-jmp $
+jmp $                           ; Endless loop
 
-times 510-($-$$) nop
-dw 0xAA55
+times 510-($-$$) nop            ; Add lots of nops (byte 0x90)
+dw 0xAA55                       ; Add the bootloader signature
