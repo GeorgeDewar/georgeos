ef462a561c5663cc2423da6b7aa4a33fbf61963c
George Dewar <george@dewar.co.nz>
2021-09-06 22:48:08 +1200

    Tidy

--- 32bit/src/kernel/kernel.c
+++ 32bit/src/kernel/kernel.c
@@ -5,6 +5,3 @@ void _start () {
 
     }
 }
-
-// gcc -m32 -c src/kernel/kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-// ld -o build/kernel.bin -T src/kernel/linker.ld -m elf_i386 -nostdlib build/kernel.o
--- 32bit/src/kernel/kernelstub.asm
+++
@@ -1,5 +0,0 @@
-BITS 32
-org 20000h
-    %define VIDEO_MEMORY        0xb8000
-    ; mov byte [VIDEO_MEMORY], 'X'
-    jmp $
