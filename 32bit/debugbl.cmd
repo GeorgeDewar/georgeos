476136ac60ab43bf16e85a227f7420b03ec17a81
George Dewar <george@dewar.co.nz>
2021-09-11 20:24:53 +1200

    Remove redundant scripts

--- 32bit/debugbl.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-gdb -ex "target remote localhost:1234" ^
-    -ex "set architecture i8086" ^
-    -ex "break *0x%1" ^
-    -ex "continue" ^
-    -ex "display/3i $pc" ^
-    -ex "x/3i $pc"
