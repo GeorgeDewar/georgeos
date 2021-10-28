01d61854e10864133e9c94f6ae162827e3ba5f20
George Dewar <george@dewar.co.nz>
2021-10-28 22:45:38 +1300

    Redefine uint32_t

--- 32bit/src/kernel/include/system.h
+++ 32bit/src/kernel/include/system.h
@@ -6,7 +6,7 @@ typedef long long int64_t;
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
-typedef unsigned long uint32_t;
+typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 
 typedef unsigned long size_t;
