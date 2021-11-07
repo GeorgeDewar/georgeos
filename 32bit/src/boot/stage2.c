f960e0b4cb71eb9d45b9f8e24ebea95e8a09f9eb
George Dewar <george@dewar.co.nz>
2021-11-07 21:47:12 +1300

    Stage2 kinda working, but does not disassemble correctly

--- 32bit/src/boot/stage2.c
+++ 32bit/src/boot/stage2.c
@@ -1,3 +1,23 @@
+void print_string(char *str);
+void print_char(char c);
+
 void main() {
+    print_string("Stage2!");
     for(;;);
+}
+
+void print_string(char *str) {
+    while(*str != 0) {
+        print_char(*str++);
+    }
+}
+
+void print_char(char c) {
+    asm(
+        "mov %0, %%al;"
+        "mov $0x0E, %%ah;"
+        "int $0x10;"
+        : // no output
+        : "r" (c) // %0 = c
+    );
 }
---
+++ 32bit/src/boot/stage2_entry.asm
@@ -0,0 +1,7 @@
+[bits 16]
+[extern main]
+
+global _start
+_start: ; ld wants to see this label - without it it will work, but emit a warning
+    call main
+    jmp $
