0299da3d9ecfb11315d4a9840e3c9d829d5f2f42
George Dewar <george@dewar.co.nz>
2021-11-13 20:10:51 +1300

    VBE and better inline asm

--- 32bit/src/boot/stage2.c
+++ 32bit/src/boot/stage2.c
@@ -1,9 +1,28 @@
+#include "boot.h"
+
 void print_string(char *str);
 void print_char(char c);
 
 void main() {
-    print_string("Stage2!");
+    print_string("Stage2!\r\n");
+    vbe_info_block *infoblock = (vbe_info_block *) 0x9000;
+    uint16_t result = get_mode_list((uint16_t) infoblock);
+    if (result == 0x004f) {
+        print_string("Success\r\n");
+    } else {
+        print_string("VBE Error\r\n");
+        for(;;);
+    }
+
+    set_ds(infoblock->OemStringPtr[0])
+    char *oem
+    set_ds(0);
+
     for(;;);
+}
+
+void set_ds(uint16_t segment) {
+    asm("mov %0, ds" :: "a" (segment));
 }
 
 void print_string(char *str) {
@@ -18,6 +37,18 @@ void print_char(char c) {
         "mov $0x0E, %%ah;"
         "int $0x10;"
         : // no output
-        : "r" (c) // %0 = c
+        : "a" (c) // %0 = c
     );
+}
+
+uint16_t get_mode_list(uint16_t dest_addr) {
+    uint16_t output = 0;
+    asm(
+    "mov %0, %%di;"
+    "mov $0x4f00, %%ax;"
+    "int $0x10;"
+    : "=a" (output) // ax register
+    : "r" (dest_addr)
+    );
+    return output;
 }
